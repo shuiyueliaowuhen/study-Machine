bb589c68d1546fb184014a7409210a63ef33a6e3
songfei <784816716@qq.com>
2020-11-10 09:23:29 +0800

    贝叶斯

--- com/sf/study/bayes/bayes.py
+++ com/sf/study/bayes/bayes.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
             break
 
     vectorizer = CountVectorizer()
-    # corpus
+    # corpus存的是训练集中每封邮件的正文
     fea_train = vectorizer.fit_transform(corpus)
     print(vectorizer.get_feature_names())
     print(fea_train.toarray())
